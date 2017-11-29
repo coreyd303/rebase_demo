97cb1a16459caddbdf93138f7172161e449f2bd0
Corey Davis <cdavis@captechconsulting.com>
2017-11-29 08:13:41 -0700

    [INIT] Initialize repo This is the initial commit for this repository. This text must be line wrapped so that it fits within the github pop ups

---
+++ rebase.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+"This is a rebase playground"
+
+func doThings() {
+    print("Look ma, I can do the things!")
+}
+
+doThings()
